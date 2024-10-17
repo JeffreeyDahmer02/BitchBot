47bdfd1f76ef0c21dc8bb5f73efbad6614866ae3
JeffreeyDahmer02 <top1percentsquad@gmail.com>
2024-10-16 23:16:14 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,17 +1,11 @@
 local players, run_service, tween_service, workspace, http_service, gui_service, uis, current_list = game:GetService("Players"), game:GetService("RunService"), game:GetService("TweenService"), game:GetService("Workspace"), game:GetService("HttpService"), game:GetService("GuiService"), game:GetService("UserInputService"), {}
 local fonts = {
-    { ttf = "Proggy.ttf", json = "Proggy.json", url = "https://raw.githubusercontent.com/OxygenClub/Random-LUAS/main/Proggy.txt", name = "Proggy" },
-    { ttf = "Minecraftia.ttf", json = "Minecraftia.json", url = "https://raw.githubusercontent.com/OxygenClub/Random-LUAS/main/Minecraftia.txt", name = "Minecraftia" },
-    { ttf = "SmallestPixel7.ttf", json = "SmallestPixel7.json", url = "https://raw.githubusercontent.com/OxygenClub/Random-LUAS/main/Smallest%20Pixel.txt", name = "SmallestPixel7" },
-    { ttf = "Verdana.ttf", json = "Verdana.json", url = "https://raw.githubusercontent.com/OxygenClub/Random-LUAS/main/Verdana.txt", name = "Verdana" },
-    { ttf = "VerdanaBold.ttf", json = "VerdanaBold.json", url = "https://raw.githubusercontent.com/OxygenClub/Random-LUAS/main/Verdana%20Bold.txt", name = "VerdanaBold" },
-    { ttf = "Tahoma.ttf", json = "Tahoma.json", url = "https://raw.githubusercontent.com/OxygenClub/Random-LUAS/main/Tahoma.txt", name = "Tahoma" },
-    { ttf = "TahomaBold.ttf", json = "TahomaBold.json", url = "https://raw.githubusercontent.com/OxygenClub/Random-LUAS/main/Tahoma%20Bold.txt", name = "TahomaBold" },
+    { ttf = "Arial.ttf", json = "Arial.json", name = "Arial" },
 }
 
 for _, font in fonts do
     if not isfile(font.ttf) then
-        writefile(font.ttf, base64_decode(game:HttpGet(font.url)))
+        writefile(font.ttf, base64_decode(game:HttpGet("https://example.com/Arial.txt")))
     end
 
     if not isfile(font.json) then
@@ -28,24 +22,18 @@ for _, font in fonts do
         }
         writefile(font.json, game:GetService("HttpService"):JSONEncode(fontConfig))
     end
-
-
 end
+
 print("wayponint two")
 
 local DrawingFontsEnum = {
-    [0] = Font.new(getcustomasset("Verdana.json"), Enum.FontWeight.Regular),
-    [1] = Font.new(getcustomasset("SmallestPixel7.json"), Enum.FontWeight.Regular),
-    [2] = Font.new(getcustomasset("Proggy.json"), Enum.FontWeight.Regular),
-    [3] = Font.new(getcustomasset("Minecraftia.json"), Enum.FontWeight.Regular),
-    [4] = Font.new(getcustomasset("VerdanaBold.json"), Enum.FontWeight.Regular),
-    [5] = Font.new(getcustomasset("Tahoma.json"), Enum.FontWeight.Regular),
-    [6] = Font.new(getcustomasset("TahomaBold.json"), Enum.FontWeight.Regular),
+    [0] = Font.new(getcustomasset("Arial.json"), Enum.FontWeight.Regular),
 }
 
 function getfontfromindex(fontIndex)
     return DrawingFontsEnum[fontIndex]
 end
+
 
 local Library = {
 		Open = true;
